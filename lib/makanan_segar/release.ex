ca1a0ec290d3e33f3f379d4648535914d9254844
rroslan <rroslan@gmail.com>
2025-08-01 13:50:15 +0800

    edit release

--- lib/makanan_segar/release.ex
+++ lib/makanan_segar/release.ex
@@ -1,17 +1,28 @@
 defmodule MakananSegar.Release do
+  @moduledoc """
+  Used for executing DB release tasks when run in production without Mix
+  installed.
+  """
   @app :makanan_segar
 
   def migrate do
-    Application.load(@app)
+    load_app()
 
-    for repo <- Application.fetch_env!(@app, :ecto_repos) do
-      {:ok, _pid} = repo.start_link(pool_size: 2)
-      Ecto.Migrator.run(repo, migrations_path(repo), :up, all: true)
+    for repo <- repos() do
+      {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :up, all: true))
     end
   end
 
-  defp migrations_path(repo) do
-    priv_dir = Application.app_dir(@app, "priv")
-    Path.join([priv_dir, repo |> Module.split() |> List.last() |> Macro.underscore(), "migrations"])
+  def rollback(repo, version) do
+    load_app()
+    {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :down, to: version))
+  end
+
+  defp repos do
+    Application.fetch_env!(@app, :ecto_repos)
+  end
+
+  defp load_app do
+    Application.ensure_loaded(@app)
   end
 end
